a89c8e87e91917fae2f615839a0d052fb46568b5
Khalid Mohammad <khalid.m.sheet@gmail.com>
2023-06-08 01:01:04 +0300

    added guard to checkout child routes

--- src/app/modules/checkout/checkout-routing.module.ts
+++ src/app/modules/checkout/checkout-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckoutComponent } from './checkout.component';
 import { ThankYouComponent } from './thank-you/thank-you.component';
+import { checkoutGuard } from 'src/app/guards/checkout-guard.guard';
 
 const routes: Routes = [
-  { path: '', component: CheckoutComponent },
+  {
+    path: '',
+    component: CheckoutComponent,
+    canActivate: [checkoutGuard],
+  },
   {
     path: 'thank-you',
     component: ThankYouComponent,
+    canActivate: [checkoutGuard],
   },
 ];
 
